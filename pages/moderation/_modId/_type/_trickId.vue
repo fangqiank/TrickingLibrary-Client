b16442c234fd58483b861ae81c6531ea7fa374e3
fangqiank <fangqiank@gmail.com>
2021-06-23 14:16:03 -0600

    Commit on 2021/6/23 second

--- pages/moderation/_modId/_type/_trickId.vue
+++ pages/moderation/_modId/_type/_trickId.vue
@@ -3,11 +3,24 @@
     <div v-if="item">
       {{ item.description }}
     </div>
+
+    <div v-if="replyId">
+         Replying to {{replyId}} <v-btn @click="replyId=0">Clear</v-btn>
+    </div>
+
     <div>
       <v-text-field label="Comment" v-model="comment"></v-text-field>
       <v-btn @click="handleSend">Send</v-btn>
     </div>
-    <div v-for="(c,index) in comments" v-html="c.htmlContent" :key="index"></div>
+
+    <div v-for="(c,index) in comments" :key="index" class="my-1">
+      <span v-html="c.htmlContent"></span>
+      <v-btn small @click = "replyId=c.id">Reply</v-btn>
+      <v-btn small @click = "handleLoadReplies(c)">Load Replies</v-btn>
+      <div v-for="(r,idx) in c.replies" :key="idx">
+        <span v-html="r.htmlContent"></span>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -17,6 +30,8 @@ import https from "https";
 const agent = new https.Agent({
   rejectUnauthorized: false,
 });
+
+const commentWithReplies = cmt =>({...cmt, replies:[]})
 
 const endpointResolever = (type) => {
   if (type === "trick") {
@@ -28,7 +43,8 @@ export default {
   data: () => ({
     item: null,
     comments:[],
-    comment:''
+    comment:'',
+    replyId:0,
   }),
 
   created() {
@@ -42,23 +58,46 @@ export default {
       this.$axios.$get(`/api/moderationitems/${modId}/comments`,{
         httpsAgent: agent}
       )
-      .then(res => this.comments = res )
-      console.log(this.comments)
+      .then(res => this.comments = res.map(commentWithReplies))
   },
 
   methods:{
+    handleLoadReplies(comment){
+      this.$axios.$get(`/api/comments/${comment.id}/replies`,
+          {httpsAgent: agent}
+      )
+      .then(comments=>this.$set(comment,'replies',comments))
+    },
+
     handleSend(){
       const { modId } = this.$route.params
 
-      this.$axios.$post(`/api/moderationitems/${modId}/comments`,
-        {content: this.comment},
-        {httpsAgent: agent}
-      )
-      .then(x => this.comments.push(x))
+      if(this.replyId > 0){
+        this.$axios.$post(`/api/comments/${this.replyId}/replies`,
+          {content: this.comment},
+          {httpsAgent: agent}
+        )
+        .then(x => {
+          this.comments.find(x=>x.id === this.replyId).replies.push(x)
+        })
+        
+        this.comment = ''
+        console.log(this.comments)
+      } 
+      else{
+        this.$axios.$post(`/api/moderationitems/${modId}/comments`,
+          {content: this.comment},
+          {httpsAgent: agent}
+        )
+        .then(x => this.comments.push(commentWithReplies(x)))
+        
+        this.comment = ''
+      }
+      
     }
   },
   
-};
+};  
 </script>
 
 <style>
